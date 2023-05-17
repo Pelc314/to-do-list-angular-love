feae8f0b08dbc07ff4ca4d3932ed9e48e6a97405
Maciej Pelc <78445032+Pelc314@users.noreply.github.com>
2023-05-17 10:57:36 +0200

    added selections to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { AddTodo } from './todo.actions';
+import { TodoSelectors } from './todo.selectors';
+import { Observable } from 'rxjs';
+import { TodoModel } from 'src/todo-state.model';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,15 @@ import { AddTodo } from './todo.actions';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+ @Select(TodoSelectors.items)
+ items$!: Observable<TodoModel[]>;
+
+ @Select(TodoSelectors.doneItems)
+ doneItems$!:Observable<TodoModel[]>;
+
+ @Select(TodoSelectors.activeItems)
+ activeItems$!:Observable<TodoModel[]>;
+
   title = 'to-do-list-angular-love';
   newTitle!: string;
   constructor(private store: Store) { }
