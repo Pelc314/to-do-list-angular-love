3fdf7785d658dbd04af47ddf82fe4b2e00268860
Maciej Pelc <78445032+Pelc314@users.noreply.github.com>
2023-05-17 10:55:31 +0200

    Added selectors

---
+++ src/app/todo.selectors.ts
@@ -0,0 +1,20 @@
+import { TodoStateModel } from "src/todo-state.model";
+import { TodoState } from "./todo.state";
+import { Selector } from "@ngxs/store";
+
+export class TodoSelectors {
+    @Selector([TodoState])
+    static items(state: TodoStateModel) {
+        return state.items;
+    }
+
+    @Selector([TodoState])
+    static doneItems(state: TodoStateModel) {
+        return state.items.filter((it) => !it.isActive);
+    }
+
+    @Selector([TodoState])
+    static activeItems(state:TodoStateModel){
+        return state.items.filter((it)=> it.isActive); 
+    }
+}
